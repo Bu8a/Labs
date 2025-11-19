d7d7ea6e5d38effda2e2e908f41cd955a254b614
Bu8a <kekbirkek@gmail.com>
2025-11-19 18:01:00 +0400

    lab4 .py

--- lab4 .py
+++ lab4 .py
@@ -1,12 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def load_matrix(filename):
-    return np.loadtxt(filename, dtype=int)
 
 def print_matrix(matrix, name):
     print(f"\n{name}:")
-    print(matrix)
+    with np.printoptions(suppress=True, precision=4):
+        print(matrix)
 
 def split_blocks(A):
     h = A.shape[0] // 2
@@ -19,11 +18,12 @@ def split_blocks(A):
 def build_F(A):
     F = A.copy()
     D, E, C, B = split_blocks(A)
+    h = D.shape[0]
     
     zeros_perimeter_C = np.sum(C[0, :] == 0) + np.sum(C[-1, :] == 0) + \
                         np.sum(C[:, 0] == 0) + np.sum(C[:, -1] == 0)
     
-    perimeter_values_C = list(map(int, list(C[0, :]) + list(C[-1, :]) + list(C[:, 0]) + list(C[:, -1])))
+    perimeter_values_C = C[[0, 0, -1, -1], [0, -1, 0, -1]]
     product_perimeter_C = 1
     for value in perimeter_values_C:
         if value != 0:
@@ -34,11 +34,11 @@ def build_F(A):
     print(f"Произведение чисел на периметре области C: {product_perimeter_C}")
 
     if zeros_perimeter_C > product_perimeter_C:
-        print("Меняем симметрично области E и B")
-        F[:E.shape[0], :E.shape[1]], F[:B.shape[0], B.shape[1]:] = B, E
+        print("Меняем симметрично области D и B")
+        F[:h, :h], F[h:, h:] = B, D
     else:
-        print("Меняем несимметрично области B и C")
-        F[:B.shape[0], B.shape[1]:], F[C.shape[0]:, C.shape[1]:] = C, B
+        print("Меняем несимметрично области C и B")
+        F[h:, :h], F[h:, h:] = B, C
     
     return F
 
@@ -50,7 +50,15 @@ def compute_result(A, F, K):
         else:
             print("\nОпределитель A меньше или равен сумме диагональных элементов F")
             G = np.tril(A)
-            return (np.linalg.inv(A) + G - np.linalg.inv(F)) * K
+
+            det_A = np.linalg.det(A)
+            det_F = np.linalg.det(F)
+
+            if np.isclose(det_A, 0.0) or np.isclose(det_F, 0.0):
+                print(f" Матрица A (det={det_A:.2e}) или F (det={det_F:.2e}) сингулярна. Вычисление (A^-1 + G - F^-1) * K невозможно.")
+                return np.zeros_like(A)
+
+        return (np.linalg.inv(A) + G - np.linalg.inv(F)) * K
 
 
 def plot_graphs(F):
@@ -74,7 +82,10 @@ def plot_graphs(F):
 
 def main():
     K = int(input("Введите K: "))
-    A = load_matrix('matrix_data.txt')
+    N_possible_sizes = np.arange(4, 10, 2)
+    N = np.random.choice(N_possible_sizes)
+    print(f"Случайный размер матрицы N: {N}x{N}")
+    A = np.random.randint(-10, 11, size=(N, N))
     print_matrix(A, "Матрица A")
     
     F = build_F(A)
